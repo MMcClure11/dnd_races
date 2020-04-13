327a32a1633e9176443b80e9e1d75997d951ddbb
MMcClure11 <mcclme02@luther.edu>
2020-04-13 12:00:43 -0500

    adds making network request to api

--- lib/dnd_races/api_manager.rb
+++ lib/dnd_races/api_manager.rb
@@ -3,11 +3,13 @@ class DndRaces::APIManager
 BASE_URL = "https://www.dnd5eapi.co"
 
     def self.get_races
+        puts "MAKING NETWORK REQUEST......\n\n"
         res = HTTParty.get(BASE_URL + "/api/races")
         DndRaces::Race.create_from_api(res["results"])
     end
 
     def self.get_info_about(race)
+        puts "\n\nMAKING NETWORK REQUEST......\n\n"
         res = HTTParty.get(BASE_URL + "#{race.url}")
         race.speed = res["speed"]
         race.alignment = res["alignment"]
